dc07ad1ea0817be1fb0ecfddab16cc08fdb4b2d3
dumitreu <157018890+dumitreu@users.noreply.github.com>
2024-02-09 14:49:52 +0200

    Update logging.hpp

--- src/logging.hpp
+++ src/logging.hpp
@@ -15,7 +15,7 @@ namespace logging {
 
 }
 #ifdef PLATFORM_WINDOWS
-#define LOG (****logging::log_output) << lins::timespec_wrapper::now().to_iso_8601_str(3) << " in file " << lins::file_util::extract_file_name(__FILE__) << ":" << __LINE__ << ", " << __FUNCSIG__
+#define LOG (****logging::log_output) << lins::timespec_wrapper::now().to_iso_8601_str(3) << " in file " << lins::file_util::extract_file_name(__FILE__) << ":" << __LINE__ << ", " << __FUNCSIG__ << ": "
 #else
-#define LOG (****logging::log_output) << lins::timespec_wrapper::now().to_iso_8601_str(3) << " in file " << lins::file_util::extract_file_name(__FILE__) << ":" << __LINE__ << ", " << __PRETTY_FUNCTION__
+#define LOG (****logging::log_output) << lins::timespec_wrapper::now().to_iso_8601_str(3) << " in file " << lins::file_util::extract_file_name(__FILE__) << ":" << __LINE__ << ", " << __PRETTY_FUNCTION__ << ": "
 #endif
